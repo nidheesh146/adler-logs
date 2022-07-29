fc82287258a73df0435682bc0c5493bea0e46a7b
shilma <shilma33@gmail.com>
2022-07-29 11:56:12 +0300

    mrp label modification

--- resources/views/pages/label/patient-label-print.blade.php
+++ resources/views/pages/label/patient-label-print.blade.php
@@ -27,10 +27,10 @@
             </div>
             <div class="row label-div columns" id="label-div">
                 @for ($i = 0; $i< $no_of_label; $i++)
-                <div class=" column" style=" height:6.57%;float: left;width: 29%;border-style: ridge;border-color:#f0f0f5 ;margin:5px;padding:1px;">
+                <div class=" column" style=" height:6.599%;float: left;width: 28%;border-style: ridge;border-color:#f0f0f5 ;margin:5px;padding:1.5px;">
                         <div class="sub-columns" >
                             <div class="" style="width:5%;text-align: justify;">
-                                <span class="smalltext" style="font-size:5px;float: left;writing-mode: vertical-lr;transform: rotate(180deg);">
+                                <span class="smalltext" style="font-size:5px;float: left;writing-mode: vertical-lr;transform: rotate(180deg); margin-top:20%;">
                                 LBL/F-10_REV00_{{date("d-M-y")}}
                                 </span>
                             </div>
