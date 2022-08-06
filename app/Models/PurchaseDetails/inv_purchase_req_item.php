336836c4c6427136385e17d9fe9b2cf57eda20c0
baiju <baiju@skyplan.com>
2022-08-06 15:33:57 +0530

    req approval

--- app/Models/PurchaseDetails/inv_purchase_req_item.php
+++ app/Models/PurchaseDetails/inv_purchase_req_item.php
@@ -37,7 +37,10 @@ class inv_purchase_req_item extends Model
 
     function getItem($condition){
         return  $this->select(['inv_purchase_req_item.requisition_item_id','inv_supplier.vendor_id','inv_supplier.vendor_name','inv_purchase_req_item.actual_order_qty','inv_purchase_req_item.rate',
-        'inv_purchase_req_item.discount_percent','inventory_gst.gst','currency_exchange_rate.currency_code','inv_purchase_req_item.net_value','inv_purchase_req_item.basic_value','inventory_rawmaterial.item_code','inventory_rawmaterial.discription','inventory_rawmaterial.hsn_code', 'inventory_rawmaterial.availble_quantity', 'inventory_rawmaterial.opening_quantity', 'inventory_rawmaterial.max_stock', 'inventory_rawmaterial.min_stock', 'inv_item_type.type_name','inv_item_type.id as item_type_id', 'inv_purchase_req_item.remarks','inv_unit.unit_name', 'inv_unit.id as unit_id', 'inv_supplier.id as supplierId','inv_supplier.vendor_id as vendorId','inv_supplier.vendor_name as vendorName' ])
+                               'inv_purchase_req_item.discount_percent','inventory_gst.gst','currency_exchange_rate.currency_code','inv_purchase_req_item.net_value','inv_purchase_req_item.basic_value',
+                               'inventory_rawmaterial.item_code','inventory_rawmaterial.discription','inventory_rawmaterial.hsn_code', 'inventory_rawmaterial.availble_quantity', 'inventory_rawmaterial.opening_quantity',
+                                'inventory_rawmaterial.max_stock', 'inventory_rawmaterial.min_stock', 'inv_item_type.type_name','inv_item_type.id as item_type_id', 'inv_purchase_req_item.remarks','inv_unit.unit_name', 
+                                'inv_unit.id as unit_id', 'inv_supplier.id as supplierId','inv_supplier.vendor_id as vendorId','inv_supplier.vendor_name as vendorName'])
                      ->leftjoin('inv_purchase_req_master_item_rel','inv_purchase_req_master_item_rel.item','=','inv_purchase_req_item.requisition_item_id')
                      ->leftjoin('inventory_rawmaterial','inventory_rawmaterial.id','=','inv_purchase_req_item.Item_code')
                      ->leftjoin('inv_unit', 'inv_unit.id','=', 'inventory_rawmaterial.issue_unit_id')
@@ -51,7 +54,7 @@ class inv_purchase_req_item extends Model
         
     }
 
-    function getdata($condition){
+    function getdata($condition,$status){
        return  $this->select(['inv_purchase_req_item.requisition_item_id','inv_supplier.vendor_id','inv_supplier.vendor_name','inv_purchase_req_item.actual_order_qty','inv_purchase_req_item.rate',
        'inv_purchase_req_item.discount_percent','inventory_gst.gst','currency_exchange_rate.currency_code','inv_purchase_req_item.net_value','inventory_rawmaterial.item_code'])
                     ->leftjoin('inv_purchase_req_master_item_rel','inv_purchase_req_master_item_rel.item','=','inv_purchase_req_item.requisition_item_id')
@@ -59,10 +62,26 @@ class inv_purchase_req_item extends Model
                     ->leftjoin('inv_supplier','inv_supplier.id','=','inv_purchase_req_item.supplier')
                     ->leftjoin('inventory_gst','inventory_gst.id','=','inv_purchase_req_item.gst')
                     ->leftjoin('currency_exchange_rate','currency_exchange_rate.currency_id','=','inv_purchase_req_item.currency')
-                    ->whereIn('inv_purchase_req_item_approve.status',[4,5])
+                  //  ->whereIn('inv_purchase_req_item_approve.status',[4,5])
                     ->where($condition)
                     ->orderby('inv_purchase_req_item.requisition_item_id','desc')
                     ->paginate(15);
     }
+    
+    function getdata_approved($condition){
+        return  $this->select(['inv_purchase_req_item.requisition_item_id','inv_supplier.vendor_id','inv_supplier.vendor_name','inv_purchase_req_item.actual_order_qty','inv_purchase_req_item.rate',
+                               'inv_purchase_req_item.discount_percent','inventory_gst.gst','currency_exchange_rate.currency_code','inv_purchase_req_item.net_value','inventory_rawmaterial.item_code',
+                               'inv_purchase_req_master.pr_no','inv_purchase_req_item_approve.status'])
+                     ->leftjoin('inv_purchase_req_master_item_rel','inv_purchase_req_master_item_rel.item','=','inv_purchase_req_item.requisition_item_id')
+                     ->leftjoin('inv_purchase_req_master','inv_purchase_req_master.master_id','=','inv_purchase_req_master_item_rel.master')
+                     ->leftjoin('inventory_rawmaterial','inventory_rawmaterial.id','=','inv_purchase_req_item.Item_code')
+                     ->leftjoin('inv_supplier','inv_supplier.id','=','inv_purchase_req_item.supplier')
+                     ->leftjoin('inventory_gst','inventory_gst.id','=','inv_purchase_req_item.gst')
+                     ->leftjoin('currency_exchange_rate','currency_exchange_rate.currency_id','=','inv_purchase_req_item.currency')
+                     ->whereIn('inv_purchase_req_item_approve.status',[4,5])
+                     ->where($condition)
+                     ->orderby('inv_purchase_req_item.requisition_item_id','desc')
+                     ->paginate(10);
+     }
 
 }
--- resources/views/pages/purchase-details/purchase-requisition/purchase-requisition-approvel.blade.php
+++ resources/views/pages/purchase-details/purchase-requisition/purchase-requisition-approvel.blade.php
@@ -9,12 +9,12 @@
 			<h4 class="az-content-title" style="font-size: 20px;">Purchase Requisition Approval
               <div class="right-button">
                 
-                  <button data-toggle="dropdown" style="float: right; margin-left: 9px;font-size: 14px;" class="badge badge-pill badge-info ">
+                  <!-- <button data-toggle="dropdown" style="float: right; margin-left: 9px;font-size: 14px;" class="badge badge-pill badge-info ">
                       <i class="fa fa-download" aria-hidden="true"></i> Download <i class="icon ion-ios-arrow-down tx-11 mg-l-3"></i></button>
                   <div class="dropdown-menu">
                   <a href="" class="dropdown-item">Excel</a>
           
-                  </div>
+                  </div> -->
               <div>  
               </div>
           </div>
@@ -22,12 +22,13 @@
 			<div class="az-dashboard-nav">
 				<nav class="nav"> </nav>
 			</div>
-			@if($data['error'])
-			<div class="alert alert-danger "  role="alert" style="width: 100%;">
-				<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-				{{$data['error'] }}
-			</div>
-	       @endif
+
+			@foreach ($errors->all() as $errorr)
+                    <div class="alert alert-danger "  role="alert" style="width: 100%;">
+                       <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                      {{ $errorr }}
+                    </div>
+                   @endforeach 
 		   
 		   @if (Session::get('success'))
 		   <div class="alert alert-success " style="width: 100%;">
@@ -39,7 +40,6 @@
 				<table class="table table-bordered mg-b-0" id="example1">
 					<thead>
 						<tr>
-							<th style="display:none;"></th>
 						    <th>PR NO:</th>
 							<th>Item code </th>
 							<th>Supplier</th>
@@ -54,47 +54,48 @@
 						</tr>
 					</thead>
 					<tbody >
-						@if(!empty($data['response']['purchase_requisition'][0]))
-						@foreach($data['response']['purchase_requisition'] as $item)
+				
+						@foreach($data['inv_purchase'] as $item)
 						
-						<tr>
-							<td style="display:none;">{{ $item['id']}}</td>
-							<td>{{!empty($item['purchase_reqisition']) ? $item['purchase_reqisition']['pr_no'] : '-'}}</td>
-							<td>{{$item['item_code']['item_code']}}</td>
-							<td>{{$item['supplier']['vendor_name']}}</td>
+						<tr style="	@if($item['status'] == 5)
+							       background: #ffc1074f;
+								@endif">
+							<td>{{$item['pr_no']}}</td>
+							<td>{{$item['item_code']}}</td>
+							<td><span>{{$item['vendor_id']}}</span></td>
 							<td>{{$item['actual_order_qty']}}</td>
 						    <td>{{$item['rate']}}</td>
 							<td>{{$item['discount_percent']}}</td>
 							<td>{{$item['gst']}}</td>
-							<td>{{$item['currency']}}</td>
+							<td>{{$item['currency_code']}}</td>
 							<td>{{$item['net_value']}}</td>
 							{{-- <td><span class="badge badge-pill badge-info ">waiting for Action<span></td> --}}
 							<td>
-							<a href="#" data-toggle="modal" value="{{$item['id']}}" rel="{{$item['item_code']['item_code']}}" data-target="#myModal" id="change-status" style="width: 64px;" 
+							<a href="#" data-toggle="modal" value="{{$item['requisition_item_id']}}" rel="{{$item['vendor_id']}}" data-target="#myModal" id="change-status" style="width: 64px;" 
 							data-html="true" data-placement="top" 
 							class="badge 
-							@if($item['status'] == 0)
+							@if($item['status'] == 4)
 							    badge-info
-								@elseif($item['status'] == 3)
+								@elseif($item['status'] == 5)
 								badge-warning
 								@endif
 							">
-							@if($item['status'] == 0)
+							@if($item['status'] ==4)
 							    Pending
-							@elseif($item['status'] == 3)
+							@elseif($item['status'] == 5)
 								On hold
 							@endif
 							</a></td>
 						</tr>	
 					
 						@endforeach
-						@endif
+					
 					</tbody>
 				</table>
 
-				@if(!empty($data['response']))
-				   @include('includes.pagination',['data'=>$data['response']])
-				@endif
+				<div class="box-footer clearfix">
+					{{ $data['inv_purchase']->appends(request()->input())->links() }}
+				</div>
 		
 			</div>
 		</div>
@@ -118,7 +119,7 @@
 									<option value=""> --Select One-- </option>
 									{{-- <option value="0"> Pending </option> --}}
 									<option value="1"> Approve</option>
-									<option value="3">On hold</option>
+									<option value="5">On hold</option>
                                 </select>
                             </div>
 							<div class="form-group">
@@ -132,7 +133,7 @@
                             
                         </div>
                         <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <!-- <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button> -->
                             <button type="submit" class="btn btn-primary" id="save"><span class="spinner-border spinner-button spinner-border-sm" style="display:none;"
 								role="status" aria-hidden="true"></span> <i class="fas fa-save"></i> Submit</button>
                         </div>
@@ -177,7 +178,7 @@
                     required: true,
                 },
                 approved_qty: {
-                    required: true,
+                    // required: true,
 					number: true,
                 },
                 reason: {
@@ -199,7 +200,6 @@ $(document).ready(function() {
 			$(".item-codes").text('') ;
 			let purchaseRequisitionItemId = $(this).attr('value');
 			$('#purchaseRequisitionItemId').val(purchaseRequisitionItemId);
-
 			$(".item-codes").text('( '+ $(this).attr('rel') + ')');
         });
         
