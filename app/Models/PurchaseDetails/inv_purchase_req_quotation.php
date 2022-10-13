734adfca38739842599981e3bd13a3d5d448010c
Baiju <baijumca005@gmail.com>
2022-10-14 00:30:47 +0530

    emailchanges

--- app/Models/PurchaseDetails/inv_purchase_req_quotation.php
+++ app/Models/PurchaseDetails/inv_purchase_req_quotation.php
@@ -40,7 +40,7 @@ class inv_purchase_req_quotation extends Model
                     $mailData = new \stdClass();
                     $mailData->module = 'add_quotation';
                     $mailData->subject = "Adler";
-                    $mailData->to = ['baijumca005@gmail.com'];
+                    $mailData->to = ['baijumca005@gmail.com','komal.murali@gmail.com'];
                     $supp = DB::table('inv_supplier')->select(['vendor_id','vendor_name','email'])->where(['id'=>$supplier_id])->first();
                     //$mailData->to =   json_decode($supp->email,true);
                     $mailData->vendor_id = $supp->vendor_id;
