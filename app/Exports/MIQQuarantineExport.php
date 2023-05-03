641d98bbc89aceaa1e2510e3f0713db741b70928
SREYA97K <sreya97k@gmail.com>
2023-05-03 12:12:32 +0530

    quarantine report

--- app/Exports/MIQQuarantineExport.php
+++ app/Exports/MIQQuarantineExport.php
@@ -8,7 +8,7 @@ use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithStyles;
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 use Maatwebsite\Excel\Concerns\WithEvents;
-use Maatwebsite\Excel\Events\AfterSheet;
+use Maatwebsite\Excel\Events\AfterSheet; 
 
 class MIQQuarantineExport implements FromCollection, WithHeadings, WithStyles,WithEvents
 {
@@ -22,13 +22,15 @@ class MIQQuarantineExport implements FromCollection, WithHeadings, WithStyles,Wi
     {
         if($this->request=='null')
         {
-            $items =inv_miq::select('inv_miq.id as miq_id','inv_miq.miq_number','inv_miq.miq_date','inv_supplier_invoice_master.invoice_number','inventory_rawmaterial.item_code','inventory_rawmaterial.discription','inventory_rawmaterial.hsn_code','inv_item_type.type_name','user.f_name','user.l_name','inv_miq_item.*','inv_final_purchase_order_master.po_number','inv_supplier_invoice_master.invoice_date','inv_supplier.vendor_id','inv_supplier.vendor_name','inv_miq_item.value_inr','currency_exchange_rate.currency_code',
-                    'inv_miq_item.conversion_rate')
+             $items =inv_miq::select('inv_miq_item.id as item_id','inv_miq_item.expiry_control','inv_miq_item.expiry_date','inv_supplier_invoice_item.order_qty','inv_supplier_invoice_item.rate','inv_supplier_invoice_item.discount',
+                    'inventory_rawmaterial.item_code','inventory_rawmaterial.discription','inventory_rawmaterial.hsn_code','inv_item_type.type_name','inv_unit.unit_name','inv_lot_allocation.lot_number','inv_miq_item.value_inr','currency_exchange_rate.currency_code',
+                    'inv_miq_item.conversion_rate','inv_miq.miq_number','inv_miq.miq_date','inv_miq.created_at','user.f_name','user.l_name','inv_supplier.vendor_id','inv_supplier.vendor_name','inv_supplier_invoice_master.invoice_number','inv_supplier_invoice_master.invoice_date','inv_final_purchase_order_master.po_number')
                     ->leftjoin('inv_miq_item_rel','inv_miq_item_rel.master','=','inv_miq.id')
                     ->leftjoin('inv_miq_item','inv_miq_item.id','=','inv_miq_item_rel.item')
                     ->leftjoin('inv_supplier_invoice_master','inv_supplier_invoice_master.id','inv_miq.invoice_master_id')
                     ->leftjoin('inv_supplier_invoice_rel','inv_supplier_invoice_rel.master','inv_supplier_invoice_master.id')
                     ->leftjoin('inv_supplier_invoice_item','inv_supplier_invoice_item.id','inv_supplier_invoice_rel.item')
+                    ->leftjoin('inv_lot_allocation','inv_lot_allocation.si_invoice_item_id','=','inv_supplier_invoice_item.id')
                     ->leftjoin('inv_purchase_req_item','inv_purchase_req_item.requisition_item_id','=','inv_supplier_invoice_item.item_id')
                     ->leftjoin('inventory_rawmaterial','inventory_rawmaterial.id','=','inv_purchase_req_item.item_code')
                     ->leftjoin('inv_item_type','inv_item_type.id','=','inventory_rawmaterial.item_type_id')
@@ -67,13 +69,15 @@ class MIQQuarantineExport implements FromCollection, WithHeadings, WithStyles,Wi
                 $condition[] = ['inv_miq.miq_date', '>=', date('Y-m-d', strtotime('01-' . $this->request->from))];
                 $condition[] = ['inv_miq.miq_date', '<=', date('Y-m-t', strtotime('01-' . $this->request->from))];
             }
-           $items =inv_miq::select('inv_miq.id as miq_id','inv_miq.miq_number','inv_miq.miq_date','inv_supplier_invoice_master.invoice_number','inventory_rawmaterial.item_code','inventory_rawmaterial.discription','inventory_rawmaterial.hsn_code','inv_item_type.type_name','user.f_name','user.l_name','inv_miq_item.*','inv_final_purchase_order_master.po_number','inv_supplier_invoice_master.invoice_date','inv_supplier.vendor_id','inv_supplier.vendor_name','inv_miq_item.value_inr','currency_exchange_rate.currency_code',
-                    'inv_miq_item.conversion_rate')
+             $items =inv_miq::select('inv_miq_item.id as item_id','inv_miq_item.expiry_control','inv_miq_item.expiry_date','inv_supplier_invoice_item.order_qty','inv_supplier_invoice_item.rate','inv_supplier_invoice_item.discount',
+                    'inventory_rawmaterial.item_code','inventory_rawmaterial.discription','inventory_rawmaterial.hsn_code','inv_item_type.type_name','inv_unit.unit_name','inv_lot_allocation.lot_number','inv_miq_item.value_inr','currency_exchange_rate.currency_code',
+                    'inv_miq_item.conversion_rate','inv_miq.miq_number','inv_miq.miq_date','inv_miq.created_at','user.f_name','user.l_name','inv_supplier.vendor_id','inv_supplier.vendor_name','inv_supplier_invoice_master.invoice_number','inv_supplier_invoice_master.invoice_date','inv_final_purchase_order_master.po_number')
                     ->leftjoin('inv_miq_item_rel','inv_miq_item_rel.master','=','inv_miq.id')
                     ->leftjoin('inv_miq_item','inv_miq_item.id','=','inv_miq_item_rel.item')
                     ->leftjoin('inv_supplier_invoice_master','inv_supplier_invoice_master.id','inv_miq.invoice_master_id')
                     ->leftjoin('inv_supplier_invoice_rel','inv_supplier_invoice_rel.master','inv_supplier_invoice_master.id')
                     ->leftjoin('inv_supplier_invoice_item','inv_supplier_invoice_item.id','inv_supplier_invoice_rel.item')
+                     ->leftjoin('inv_lot_allocation','inv_lot_allocation.si_invoice_item_id','=','inv_supplier_invoice_item.id')
                     ->leftjoin('inv_purchase_req_item','inv_purchase_req_item.requisition_item_id','=','inv_supplier_invoice_item.item_id')
                     ->leftjoin('inventory_rawmaterial','inventory_rawmaterial.id','=','inv_purchase_req_item.item_code')
                     ->leftjoin('inv_item_type','inv_item_type.id','=','inventory_rawmaterial.item_type_id')
@@ -182,28 +186,29 @@ class MIQQuarantineExport implements FromCollection, WithHeadings, WithStyles,Wi
                 
                 $event->sheet->getDelegate()->getColumnDimension('A')->setWidth(5);
                 $event->sheet->getDelegate()->getColumnDimension('B')->setWidth(18);
-                $event->sheet->getDelegate()->getColumnDimension('C')->setWidth(15);
-                $event->sheet->getDelegate()->getColumnDimension('D')->setWidth(15);
-                $event->sheet->getDelegate()->getColumnDimension('E')->setWidth(10);
+                $event->sheet->getDelegate()->getColumnDimension('C')->setWidth(18);
+                $event->sheet->getDelegate()->getColumnDimension('D')->setWidth(18);
+                $event->sheet->getDelegate()->getColumnDimension('E')->setWidth(12);
                 $event->sheet->getDelegate()->getColumnDimension('F')->setWidth(15);
                 $event->sheet->getDelegate()->getColumnDimension('G')->setWidth(15);
                 $event->sheet->getDelegate()->getColumnDimension('H')->setWidth(15);
-                $event->sheet->getDelegate()->getColumnDimension('I')->setWidth(60);
-                $event->sheet->getDelegate()->getColumnDimension('J')->setWidth(10);
-                $event->sheet->getDelegate()->getColumnDimension('K')->setWidth(20);
+                $event->sheet->getDelegate()->getColumnDimension('I')->setWidth(75);
+                $event->sheet->getDelegate()->getColumnDimension('J')->setWidth(15);
+                $event->sheet->getDelegate()->getColumnDimension('K')->setWidth(25);
                 $event->sheet->getDelegate()->getColumnDimension('L')->setWidth(10);
                 $event->sheet->getDelegate()->getColumnDimension('M')->setWidth(10);
-                $event->sheet->getDelegate()->getColumnDimension('N')->setWidth(22);
+                $event->sheet->getDelegate()->getColumnDimension('N')->setWidth(10);
                 $event->sheet->getDelegate()->getColumnDimension('O')->setWidth(10);
-                $event->sheet->getDelegate()->getColumnDimension('P')->setWidth(15);
-                $event->sheet->getDelegate()->getColumnDimension('Q')->setWidth(15);
-                $event->sheet->getDelegate()->getColumnDimension('R')->setWidth(15);
-                $event->sheet->getDelegate()->getColumnDimension('S')->setWidth(15);
+                $event->sheet->getDelegate()->getColumnDimension('P')->setWidth(10);
+                $event->sheet->getDelegate()->getColumnDimension('Q')->setWidth(10);
+                $event->sheet->getDelegate()->getColumnDimension('R')->setWidth(10);
+                $event->sheet->getDelegate()->getColumnDimension('S')->setWidth(10);
                 $event->sheet->getDelegate()->getColumnDimension('T')->setWidth(15);
                 $event->sheet->getDelegate()->getColumnDimension('U')->setWidth(15);
-                $event->sheet->getDelegate()->getColumnDimension('V')->setWidth(20);
+                $event->sheet->getDelegate()->getColumnDimension('V')->setWidth(15);
                 $event->sheet->getDelegate()->getColumnDimension('W')->setWidth(15);
-                
+                $event->sheet->getDelegate()->getColumnDimension('X')->setWidth(16);
+                $event->sheet->getDelegate()->getColumnDimension('Y')->setWidth(15);
                 // $cellRange = 'F1:F20000';
                 // $event->sheet->getDelegate()->getStyle($cellRange)->getAlignment()->setWrapText(true);
             },
