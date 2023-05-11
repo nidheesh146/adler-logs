9a172443119f47fabb70be5bff6079377238bc12
shilma <shilma33@gmail.com>
2023-05-11 20:56:56 +0300

    mTQ

---
+++ app/Models/FGS/fgs_mtq.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models\FGS;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class fgs_mtq extends Model
+{
+    protected $table = 'fgs_mtq';
+    protected $primary_key = 'id';
+    protected $guarded = [];
+    public $timestamps = false;
+
+    function insert_data($data){
+        return $this->insertGetId($data);
+    }
+    function update_data($condition,$data){
+        return $this->where($condition)->update($data);
+    }
+
+    function get_single_mtq($condition) 
+    {
+        return $this->select('fgs_mtq.*','fgs_product_category.category_name','product_stock_location.location_name as location_name1', 'stock_location.location_name as location_name2')
+                    ->leftJoin('fgs_product_category','fgs_product_category.id','fgs_mtq.product_category_id')
+                    ->leftJoin('product_stock_location','product_stock_location.id','fgs_mtq.stock_location_id1')
+                    ->leftJoin('product_stock_location as stock_location','stock_location.id','fgs_mtq.stock_location_id2')
+                    ->where($condition)
+                    ->distinct('fgs_mtq.id')
+                    ->first();
+    }
+    function get_all_mtq($condition) 
+    {
+        return $this->select('fgs_mtq.*','fgs_product_category.category_name','product_stock_location.location_name as location_name1','stock_location.location_name as location_name2')
+                    ->leftJoin('fgs_product_category','fgs_product_category.id','fgs_mtq.product_category_id')
+                    ->leftJoin('product_stock_location','product_stock_location.id','fgs_mtq.stock_location_id1')
+                    ->leftJoin('product_stock_location as stock_location','stock_location.id','fgs_mtq.stock_location_id2')
+                    ->where($condition)
+                    ->where('fgs_mtq.status','=',1)
+                    ->distinct('fgs_mtq.id')
+                    ->paginate(15);
+    }
+}
---
+++ app/Models/FGS/fgs_mtq_item.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models\FGS;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use DB;
+class fgs_mtq_item extends Model
+{
+    
+    protected $table = 'fgs_mtq_item';
+    protected $primary_key = 'id';
+    protected $guarded = [];
+    public $timestamps = false;
+
+    function insert_data($data,$mtq_id){
+        $item_id =  $this->insertGetId($data);
+        if($item_id){
+            DB::table('fgs_mtq_item_rel')->insert(['master'=>$mtq_id,'item'=>$item_id]);
+        }
+        return true;
+    }
+    function update_data($condition,$data){
+        return $this->where($condition)->update($data);
+    }
+    function getMTQItems($condition)
+    {
+        return $this->select('fgs_mtq_item.*','product_product.sku_code','product_product.discription','product_product.hsn_code','batchcard_batchcard.batch_no','fgs_mtq.mtq_number')
+                        ->leftjoin('fgs_mtq_item_rel','fgs_mtq_item_rel.item','=','fgs_mtq_item.id')
+                        ->leftjoin('fgs_mtq','fgs_mtq.id','=','fgs_mtq_item_rel.master')
+                        ->leftjoin('product_product','product_product.id','=','fgs_mtq_item.product_id')
+                        ->leftjoin('batchcard_batchcard','batchcard_batchcard.id','=','fgs_mtq_item.batchcard_id')
+                        ->where($condition)
+                        ->orderBy('fgs_mtq_item.id','DESC')
+                        ->paginate(15);
+    }
+}
